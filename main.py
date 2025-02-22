287f55f33e375c6acd62acf737df202e8cccb888
Abhinav Agrawal <104521563+Engg-Abhinav@users.noreply.github.com>
2025-02-22 14:16:02 +0530

    SmartShelf AI

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+import os
+from flask import Flask, send_from_directory
 from app import create_app
 
 app = create_app()
 
+@app.route('/assets/<path:filename>')
+def serve_assets(filename):
+    return send_from_directory(os.path.join(app.static_folder, 'assets'), filename)
+
 if __name__ == '__main__':
-    # app.run(debug=True)
-    app.run(host="0.0.0.0", port=8080)
+    app.run(debug=True)
+    # app.run(host="0.0.0.0", port=8080)
